fe69a2ce8bd06cb869b723001a8ef582a97c07f4
Vizards <gvizards@gmail.com>
2016-05-29 17:08:13 +0800

    initial commit

---
+++ javascripts/typed.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Vizards on 16/5/29.
+ */
+
+$(function(){
+    $(".project-tagline").typed({
+        strings: [
+            "We are the IEC team",
+            "We attended the <strong>Baidu Institude of Front End Technology 2016</strong> ",
+            "We come from the <strong>Interactive Experience Center</strong> of <strong>UESTC</strong>",
+            "We use <strong>Python</strong> and <strong>JavaScript</strong>",
+            "Here are some projects we have completed"
+        ],
+        typeSpeed: 0,
+        contentType: 'html'
+    });
+});
