3e5d8a2934b507b08d2bda7ae9659baeca976efc
Vizards <gvizards@gmail.com>
2016-06-28 23:16:38 +0800

    fix a bug

--- javascripts/hack.js
+++ javascripts/hack.js
@@ -2,7 +2,6 @@
  * Created by Vizards on 16/6/21.
  */
 
-window.onload = function () {
-    var winH = $(window).height();
-    $(".page-header").css('height',winH+'px');
-};
+var winH = $(window).height();
+$(".page-header").css('height',winH+'px');
+
